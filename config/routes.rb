53cd334645195a440225b35f139d9d402a3d2d91
Troy Leach <troyleach29@gmail.com>
2015-02-22 14:46:18 -0600

    new stuff

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get '/products/:id'       => 'products#show'
   get '/products/:id/edit'  => 'products#edit'
   patch 'products/:id'      => 'products#update'
-  delete 'products/:id'     => 'products#destroy'
+  delete 'products/:id'      => 'products#destroy'
 
     # url => controler/action
     #look up helper methods in rails
