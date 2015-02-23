9c8c378cb18fdf55de89b712647240eec65dc6a6
Troy Leach <troyleach29@gmail.com>
2015-02-22 21:13:06 -0600

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,34 @@
 class User < ActiveRecord::Base
-  # attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :login
-  attr_accessor :login
   has_many    :orders
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  attr_accessor :login
+
+  # validates :password, presence: true, length: { minimum: 6 } if new?
+  # validates :password_confirmation, presence: true if new?
+
+         
 
 
-         # validates :username,
-         #   :presence => true,
-         #   :uniqueness => {
-         #     :case_sensitive => false
-         #   }
-
-
- def self.find_for_database_authentication(warden_conditions)
+ def self.find_first_by_auth_conditions(warden_conditions)
    conditions = warden_conditions.dup
    if login = conditions.delete(:login)
-     where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+     where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
    else
-     where(conditions.to_h).first
+     if conditions[:username].nil?
+       where(conditions).first
+     else
+       where(username: conditions[:username]).first
+     end
    end
  end
+
+ validates :username,
+   :presence => true,
+   :uniqueness => {
+     :case_sensitive => false
+   }
+   
 end
